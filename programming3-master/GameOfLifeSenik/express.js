301cd6a0d3f8bc38f622f6ac3a353dfc56654b4d
Senik2010 <152201884+Senik2010@users.noreply.github.com>
2023-12-08 18:26:08 +0400

    Add files via upload

--- programming3-master/GameOfLifeSenik/express.js
+++ programming3-master/GameOfLifeSenik/express.js
@@ -27,11 +27,45 @@
 
 
 
-var fs = require('fs');
-function main(){
-fs.writeFile("hello.txt", "Hello world\n", function(err){
-console.log("fs.writeFile ended");
-});
-console.log("fs.writeFile");
-}
-main();
+// var fs = require('fs');
+// function main(){
+// fs.writeFile("hello.txt", "Hello world\n", function(err){
+// console.log("fs.writeFile ended");
+// });
+// console.log("fs.writeFile");
+// }
+// main();
+
+
+
+// N1
+
+// var express = require("express");
+// var app = express();
+
+// app.use(express.static("your_project_folder_name"));
+
+// app.get("/", function(req, res) {
+//     res.redirect("http://google.com");
+// });
+
+// app.listen(3000, function() {
+//     console.log("Example is running on port 3000");
+// });
+
+
+// N2
+
+// var express = require("express");
+// var app = express();
+
+// app.use(express.static("your_project_folder_name"));
+
+// app.get("/", function(req, res, search) {
+//     search = "tumo"
+//     res.redirect("http://google.com/search?q=" + search);
+// });
+
+// app.listen(3000, function() {
+//     console.log("Example is running on port 3000");
+// });
